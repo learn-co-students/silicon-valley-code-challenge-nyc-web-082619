5083b598dc7e5cc66eae16469e8d097ac5405dcf
Anya <anya.s.litvinova@gmail.com>
2019-09-02 18:41:33 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,42 @@
 class FundingRound
 
+    attr_reader :startup, :venture_capitalist
+    attr_accesor :type, :investment
+
+    @@all = []
+
+    def initialize(startup, venture_capitalist, type, investment)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        @investment = investment
+        @@all << self
+
+    end
+
+
+    def self.all
+        @all
+    end
+
+    def investment=(num)
+        if num <0
+            @investment = 0
+        else
+            @investment = num.to_f
+        end
+    end
+
+
+
+
+
+
+
+
+
+
+
 end
+
+    
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,27 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+s1 = Startup.new("BeeBee", "Bee", "bee.com")
+s2 = Startup.new("Bebe", "Bob", "bebe.com")
+
+vc1 = VentureCapitalist.new("Bee", 1200000000)
+vc2 = VentureCapitalist.new("Bob", 999999)
+
+f1 = FundingRound.new(s1, vc1, "angel", 1000)
+f2 = FundingRound.new(s2, vc2, "angel", 50000)
+
+
+
+
+#Startup.sign_contract(vc, type, investment)
+#Startup.funding_rounds
+#startup.num_funding_rounds
+#Startup.total_funds
+#Startup.investors
+#Startup.big_investors
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
